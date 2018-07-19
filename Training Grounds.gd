f416173eaa19730ded221aa795b984c3179fbb3f
nirmay <nirmay.singh.lamba@gmail.com>
2018-07-19 14:50:31 -0700

    Clicker game done

---
+++ Training Grounds.gd
@@ -0,0 +1,20 @@
+extends Sprite
+var CluckBucks = 0
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _process(delta):
+	get_node("Label").set_text(str(CluckBucks))# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+	pass
+
+
+func _on_Button_pressed():
+	CluckBucks += 1
+	pass # replace with function body
